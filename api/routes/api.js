37dc43d7dcb9e830b8326f06923a45df30d3e594
gnegron72 <31548694+gnegron72@users.noreply.github.com>
2017-10-05 10:51:19 -0700

    Add route for get by id

--- api/routes/api.js
+++ api/routes/api.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 var thingController = require('../controllers/thingController.js');
 
-router.get('/thing', thingController.getThings);
+router.get('/thing', thingController.getAllThings);
+router.get('/thing/:id', thingController.getThing);
 router.post('/thing', thingController.createThing);
 router.delete('/thing/:id', thingController.deleteThing);
 
